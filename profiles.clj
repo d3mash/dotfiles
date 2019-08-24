84bce4ad7f559678c305c6fd5c914e4bfb718e0c
Demid Kashirin <demid.kashirin@gmail.com>
2019-08-24 12:57:57 +0300

    Add profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,8 @@
+{:user  {:plugins  [[cider/cider-nrepl "0.18.0"]
+                    [jonase/eastwood "0.2.5"]
+                    [venantius/ultra "0.5.1"]]
+         :dependencies [[org.clojure/tools.trace "0.7.9"]
+                        [cljfmt "0.5.7"]
+                        [jonase/eastwood "0.2.5"]
+                        [slamhound "1.5.5"]]
+         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
